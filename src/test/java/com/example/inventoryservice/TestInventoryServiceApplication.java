82a92a980a0681be7cd7f3c0671ad48b8cd2eada
I Nyoman Surya Pradipta <suryapradipta8@gmail.com>
2025-02-01 02:50:31 +0800

    Initial commit

---
+++ src/test/java/com/example/inventoryservice/TestInventoryServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(InventoryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
